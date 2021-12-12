8519c4a5aa344a96e60f2c08a64b974851f43dd8
Kenichiro Nishioka <nishioka.kenichiro@gmail.com>
2021-12-12 11:20:18 +0900

    Generate wordcloud

--- main.py
+++ main.py
@@ -5,12 +5,26 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 from googlesearch import search
+from sklearn.feature_extraction.text import TfidfTransformer
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 
 def main(keyword):
     urls = search(keyword, num_results=30)
-    df = pd.DataFrame([page_words(url) for url in urls]).fillna(0)
-    print(df.shape)
+    df = pd.DataFrame([page_words(url) for url in urls], index=urls).fillna(0)
+    tfidf = TfidfTransformer(smooth_idf=False)
+    transformed_df = pd.DataFrame(tfidf.fit_transform(df).toarray(), columns=df.columns, index=urls)
+    wc = WordCloud(background_color='white', font_path="~/Library/Fonts/ipaexg.ttf").generate_from_frequencies(
+        transformed_df.sum().to_dict())
+    fig = plt.figure(figsize=(12, 12))
+    plt.suptitle(f"Keyword: {keyword}", fontsize=20)
+    ax1 = fig.add_subplot(2, 1, 1)
+    ax1.imshow(wc)
+    ax1.axis("off")
+    ax2 = fig.add_subplot(2, 1, 2)
+    transformed_df.sum().sort_values(ascending=False).head(20).plot.bar(ax=ax2)
+    plt.savefig(f"{keyword}.png")
 
 
 def word_count(text):
